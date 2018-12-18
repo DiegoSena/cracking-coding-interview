fbea5ca47dc785bdc8fc570413c04c6ee07899cd
Diego Guimaraes <dguimaraes@fexco.com>
2018-12-18 09:38:44 +0000

    adding exercise description

--- src/main/java/com/exercises/Exercise1dot5.java
+++ src/main/java/com/exercises/Exercise1dot5.java
@@ -1,5 +1,7 @@
 package com.exercises;
 
+// There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character.
+// Given two strings, write a function to check if they are one edit (or zero edits away)
 public class Exercise1dot5 {
 
     public static void main(String[] args) {
